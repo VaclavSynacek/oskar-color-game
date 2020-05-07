0bcd493a21b3f3fb834a9ad64cee58c371c400b5
Vaclav Synacek <evasek@gmail.com>
2020-05-07 22:11:42 +0200

    mixed in cl-sdl2-mixer example 4, now can show pictures and play sound

--- main.lisp
+++ main.lisp
@@ -1,6 +1,8 @@
 #|
 
 (ql:quickload :sdl2)
+(ql:quickload :sdl2-mixer)
+(ql:quickload :sdl2)
 
 |#
 
@@ -31,21 +33,26 @@
         :right (sdl2:load-bmp "right.bmp")))
 
 (defun main()
-  (with-window-surface (window screen-surface)
-    (let* ((images (load-media))
-           (image (getf images :default)))
-      (sdl2:with-event-loop (:method :poll)
-        (:quit () t)
-        (:keydown (:keysym keysym)
-                  (case (sdl2:scancode keysym)
-                    (:scancode-up (setf image (getf images :up)))
-                    (:scancode-down (setf image (getf images :down)))
-                    (:scancode-left (setf image (getf images :left)))
-                    (:scancode-right (setf image (getf images :right)))
-                    (t (setf image (getf images :default)))))
-        (:idle ()
-               (sdl2:blit-surface image nil screen-surface nil)
-               (sdl2:update-window window)
-               (sdl2:delay 100))))))    ;reduce cpu usage
+  (sdl2:with-init (:everything)
+    (sdl2-mixer:init :ogg)
+    (sdl2-mixer:open-audio 22050 :s16sys 1 1024)
+    (sdl2-mixer:allocate-channels 1)
+    (with-window-surface (window screen-surface)
+      (let* ((images (load-media))
+             (image (getf images :default))
+             (sound-effect (sdl2-mixer:load-wav  #p"sample.ogg")))
+        (sdl2:with-event-loop (:method :poll)
+          (:quit () t)
+          (:keydown (:keysym keysym)
+                    (case (sdl2:scancode keysym)
+                      (:scancode-space (sdl2-mixer:play-channel 0 sound-effect 0))
+                      (:scancode-up (setf image (getf images :up)))
+                      (:scancode-down (setf image (getf images :down)))
+                      (:scancode-left (setf image (getf images :left)))
+                      (:scancode-right (setf image (getf images :right)))
+                      (t (setf image (getf images :default)))))
+          (:idle ()
+                 (sdl2:blit-surface image nil screen-surface nil)
+                 (sdl2:update-window window)
+                 (sdl2:delay 100)))))))    ;reduce cpu usage
 
-(main)
