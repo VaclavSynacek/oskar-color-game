194c4ef1800b4c67657c9101a35b53fcd7744599
Vaclav Synacek <evasek@gmail.com>
2020-05-11 19:25:24 +0200

    refactor to with-music-init

--- main.lisp
+++ main.lisp
@@ -34,41 +34,44 @@
 (defun load-music ()
   (list :good (sdl2-mixer:load-wav  #p"sample.ogg")))
 
-(defun close-music ()
-  (sdl2-mixer:halt-music)
-  (sdl2-mixer:close-audio)
-  ;(sdl2-mixer:free-music music)
-  (sdl2-mixer:quit)
-  t)
+(defmacro with-mixer-init (() &body body)
+  `(progn
+     (sdl2-mixer:init :ogg)
+     (sdl2-mixer:open-audio 22050 :s16sys 1 1024)
+     (sdl2-mixer:allocate-channels 1)
+     (unwind-protect
+         (progn
+           ,@body)
+      (progn
+        (sdl2-mixer:halt-music)
+        (sdl2-mixer:close-audio)
+        ;(sdl2-mixer:free-music music)
+        (sdl2-mixer:quit)))))
 
 (defun play-music (m)
   (sdl2-mixer:play-channel 0 m 0))
 
 (defun main()
   (sdl2:with-init (:everything)
-    (sdl2-mixer:init :ogg)
-    (sdl2-mixer:open-audio 22050 :s16sys 1 1024)
-    (sdl2-mixer:allocate-channels 1)
-    (with-window-surface (window screen-surface)
-      (let* ((images (load-images))
-             (image (getf images :default))
-             (musics (load-music)))
-        (sdl2:with-event-loop (:method :poll)
-          (:quit () (progn
-                      (close-music)
-                      t))
-          (:keydown (:keysym keysym)
-                    (case (sdl2:scancode keysym)
-                      (:scancode-space (play-music (getf musics :good)))
-                      (:scancode-up (setf image (getf images :up)))
-                      (:scancode-down (setf image (getf images :down)))
-                      (:scancode-left (setf image (getf images :left)))
-                      (:scancode-right (setf image (getf images :right)))
-                      (t (setf image (getf images :default)))))
-          (:idle ()
-                 (sdl2:blit-surface image nil screen-surface nil)
-                 (sdl2:update-window window)
-                 (sdl2:delay 100)))))))    ;reduce cpu usage
+    (with-mixer-init ()
+      (with-window-surface (window screen-surface)
+        (let* ((images (load-images))
+               (image (getf images :default))
+               (musics (load-music)))
+          (sdl2:with-event-loop (:method :poll)
+            (:quit () t)
+            (:keydown (:keysym keysym)
+                      (case (sdl2:scancode keysym)
+                        (:scancode-space (play-music (getf musics :good)))
+                        (:scancode-up (setf image (getf images :up)))
+                        (:scancode-down (setf image (getf images :down)))
+                        (:scancode-left (setf image (getf images :left)))
+                        (:scancode-right (setf image (getf images :right)))
+                        (t (setf image (getf images :default)))))
+            (:idle ()
+                   (sdl2:blit-surface image nil screen-surface nil)
+                   (sdl2:update-window window)
+                   (sdl2:delay 100))))))))    ;reduce cpu usage
 
 
 
